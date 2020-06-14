3d3961d7d0f7eb2c6dd6b963d86273c7e68f3e94
Lisandro Lorea <lisandrolorea@gmail.com>
2020-06-14 18:32:34 -0300

    Fix double reshape
    Sometimes (on resize) the text would get reshaped twice and breake
    This commit adds an `arabic_input` property so the `text` always
    holds the reshaped one.

--- addons/arabic-text/ALabel.gd
+++ addons/arabic-text/ALabel.gd
@@ -1,3 +1,4 @@
+tool
 extends Label
 
 var reshaper = preload('res://addons/arabic-text/reshaper/arabic_reshaper.gd').new()
@@ -5,14 +6,21 @@ var bidi = preload("res://addons/arabic-text/bidi/algorithm.gd").new()
 
 var prev_text = ''
 
+# Use this for input rather than `text`
+export(String, MULTILINE) var arabic_input = '' setget _set_arabic_input
+
+func _set_arabic_input(s):
+	arabic_input = s
+	_on_ALabel_draw()
+
 func display():
-	text = bidi.get_display(reshaper.reshape(text))
+	text = bidi.get_display(reshaper.reshape(arabic_input))
 
 func _ready():
 	display()
 	connect("draw", self, "_on_ALabel_draw")
 
 func _on_ALabel_draw():
-	if text != prev_text:
+	if arabic_input != prev_text:
 		display()
-		prev_text = text
+		prev_text = arabic_input
